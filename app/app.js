a7b3b7f72faa2bb249135ab7167fee75f6e1cdb2
unknown <soundararunachalam3639@gmail.com>
2025-09-30 10:27:20 +0530

    Delete

--- app/app.js
+++
@@ -1,5 +0,0 @@
-var app = angular.module("cup",["ngRoute"]);
-app.config(function($routeProvider){
-    $routeProvider.when("/company-wise-strategy",{
-        templateUrl:"app/view/company_wise.html"
-})})
